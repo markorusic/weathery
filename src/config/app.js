495c68d17baace48a55199bb9d102899a9385535
Marko Rusic <markousp5@gmail.com>
2019-06-30 15:46:34 +0200

    updated deps

--- src/config/app.js
+++
@@ -1,2 +0,0 @@
-export const SERVER_URL = 'https://api.darksky.net/forecast/'
-export const API_ID = 'c05684df9cac42fc903195a0c81daa83'
--- src/services/http/index.js
+++
@@ -1,8 +0,0 @@
-import { SERVER_URL, API_ID } from 'config/app'
-import axios from 'axios'
-
-const http = axios.create({
-  baseURL: `${SERVER_URL}${API_ID}/`
-})
-
-export default http
